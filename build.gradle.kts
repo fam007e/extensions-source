061d78841caae8bb5f24f6c55fac4036f6eef866
Faisal Ahmed Moshiur <19180457+fam007e@users.noreply.github.com>
2026-04-25 18:50:00 +0600

    chore: force verified patched versions for buildscript and project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,22 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://www.jitpack.io")
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            // Force these for the buildscript classpath (handles jetifier, etc)
+            force(libs.jdom2)
+            force(libs.jose4j)
+            force(libs.commons-lang3)
+            force(libs.httpclient)
+            force(libs.bc.pkix)
+            force(libs.bc.prov)
+        }
+    }
+
     dependencies {
         classpath(libs.kotlin.gradle)
 
@@ -19,16 +37,22 @@ plugins {
 allprojects {
     configurations.all {
         resolutionStrategy {
-            // Force these versions project-wide to remediate CVEs
-            // Using the catalog values ensures we are using the patched versions
+            // Force these project-wide (handles all 1,300+ modules)
             force(libs.jdom2)
             force(libs.jose4j)
-            force(libs.commons.lang3)
+            force(libs.commons-lang3)
             force(libs.httpclient)
             force(libs.httpcore)
             force(libs.bc.pkix)
             force(libs.bc.prov)
             force(libs.bc.util)
+
+            // Align all Netty modules to the same version
+            eachDependency {
+                if (requested.group == "io.netty") {
+                    useVersion(libs.versions.netty.get())
+                }
+            }
         }
     }
 }
